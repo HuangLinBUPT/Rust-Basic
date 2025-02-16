91ef40f13ff069b170619f13c04c3185f02b056f
huanglin_bupt <alin.sdr@gmail.com>
2025-02-16 16:03:10 +0800

    submit

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,18 +16,38 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-pub struct ReportCard {
-    pub grade: f32,
+
+pub trait Grade {
+    fn to_string(&self) -> String;
+}
+
+pub struct NumericGrade(f32);
+
+impl Grade for NumericGrade {
+    fn to_string(&self) -> String {
+        self.0.to_string()
+    }
+}
+
+pub struct AlphabeticGrade(String);
+
+impl Grade for AlphabeticGrade {
+    fn to_string(&self) -> String {
+        self.0.clone()
+    }
+}
+
+pub struct ReportCard<T: Grade> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: Grade> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+            &self.student_name, &self.student_age, self.grade.to_string())
     }
 }
 
@@ -38,7 +58,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: NumericGrade(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -50,9 +70,9 @@ mod tests {
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
+  
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: AlphabeticGrade("A+".to_string()),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
